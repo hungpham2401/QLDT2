d078a2126f50bd936e70aae17adbd612a80028af
hungpham2401 <86173748+hungpham2401@users.noreply.github.com>
2025-01-07 17:46:21 +0700

    update

--- shared/Tasky.Shared.Hosting/obj/Debug/net9.0/Tasky.Shared.Hosting.AssemblyInfo.cs
+++ shared/Tasky.Shared.Hosting/obj/Debug/net9.0/Tasky.Shared.Hosting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasky.Shared.Hosting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe4b98cba3c143758c5a710878da6d00fb10980")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22202e0585f2591f628004ae18d2514a162c7096")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasky.Shared.Hosting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasky.Shared.Hosting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
